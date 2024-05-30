392aa9a8b8c2c539ebd676da07a5808c66f8217c
Jakub Novak <jakub@e2b.dev>
2024-05-30 13:30:00 +0200

    Clean up start script logs

--- template/start-up.sh
+++ template/start-up.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 function start_jupyter_server() {
+	counter=0
 	response=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8888/api/status")
 	while [[ ${response} -ne 200 ]]; do
-		echo "Waiting for Jupyter Server to start..."
+	  let counter++
+	  if  (( counter % 20 == 0 )); then
+      echo "Waiting for Jupyter Server to start..."
+      sleep 0.1
+    fi
+
 		response=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8888/api/status")
 	done
 	echo "Jupyter Server started"
