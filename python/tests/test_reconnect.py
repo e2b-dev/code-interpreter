a3b9ebde6255ff5196c078dc777991952aaefc5c
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-06-27 14:28:22 -0700

    Fix reconnect test

--- python/tests/test_reconnect.py
+++ python/tests/test_reconnect.py
@@ -2,11 +2,11 @@ from e2b_code_interpreter.main import CodeInterpreter
 
 
 def test_reconnect():
-    with CodeInterpreter() as sandbox:
-        sandbox_id = sandbox.sandbox_id
-
-    sandbox = CodeInterpreter.connect(sandbox_id)
-    result = sandbox.notebook.exec_cell("x =1; x")
-    assert result.text == "1"
-
+    sandbox = CodeInterpreter()
+    sandbox_id = sandbox.sandbox_id
     sandbox.close()
+
+    sandbox2 = CodeInterpreter.connect(sandbox_id)
+    result = sandbox2.notebook.exec_cell("x =1; x")
+    sandbox2.close()
+    assert result.text == "1"
