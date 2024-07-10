e84824cea900dccbc0ebf62d93d566b84ae83060
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-07-09 22:41:28 -0700

    Fix local docker port mapping

--- template/start-up.sh
+++ template/start-up.sh
@@ -4,11 +4,11 @@ function start_jupyter_server() {
 	counter=0
 	response=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8888/api/status")
 	while [[ ${response} -ne 200 ]]; do
-	  let counter++
-	  if  (( counter % 20 == 0 )); then
-      echo "Waiting for Jupyter Server to start..."
-      sleep 0.1
-    fi
+		let counter++
+		if ((counter % 20 == 0)); then
+			echo "Waiting for Jupyter Server to start..."
+			sleep 0.1
+		fi
 
 		response=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8888/api/status")
 	done
@@ -21,14 +21,14 @@ function start_jupyter_server() {
 	fi
 
 	sudo mkdir -p /root/.jupyter
-  kernel_id=$(echo "${response}" | jq -r '.kernel.id')
+	kernel_id=$(echo "${response}" | jq -r '.kernel.id')
 	sudo echo "${kernel_id}" | sudo tee /root/.jupyter/kernel_id >/dev/null
 	sudo echo "${response}" | sudo tee /root/.jupyter/.session_info >/dev/null
 
 	cd /root/.server/
-	uvicorn main:app --port 8000  --workers 2 --no-access-log --no-use-colors
+	uvicorn main:app --host 0.0.0.0 --port 8000 --workers 2 --no-access-log --no-use-colors
 }
 
 echo "Starting Code Interpreter server..."
 start_jupyter_server &
-jupyter server --IdentityProvider.token="" > /dev/null 2>&1
+jupyter server --IdentityProvider.token="" >/dev/null 2>&1
