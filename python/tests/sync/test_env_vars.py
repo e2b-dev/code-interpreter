fe0cf2c0f147ce7311166809825de9b2c0d2ec51
Jakub Novak <jakub@e2b.dev>
2024-08-15 11:37:53 +0200

    Add missing sync test

--- python/tests/sync/test_env_vars.py
+++ python/tests/sync/test_env_vars.py
@@ -1,16 +1,31 @@
 from e2b_code_interpreter.code_interpreter_sync import CodeInterpreter
 
 
-def test_env_vars():
+async def test_env_vars_sandbox():
     sbx = CodeInterpreter(envs={"FOO": "bar"})
     result = sbx.notebook.exec_cell("import os; os.getenv('FOO')")
     assert result.text == "bar"
     sbx.kill()
 
 
-def test_env_vars_in_new_context():
-    sbx = CodeInterpreter(envs={"FOO": "bar"})
-    context_id = sbx.notebook.create_kernel()
-    result = sbx.notebook.exec_cell("import os; os.getenv('FOO')", kernel_id=context_id)
+async def test_env_vars_in_exec_cell(sandbox: CodeInterpreter):
+    result = sandbox.notebook.exec_cell("import os; os.getenv('FOO')", envs={"FOO": "bar"})
     assert result.text == "bar"
+
+
+async def test_env_vars_override():
+    sbx = CodeInterpreter(envs={"FOO": "bar", "SBX": "value"})
+    sbx.notebook.exec_cell("import os; os.setenv('FOO', 'baz'); os.setenv('RUNTIME_ENV', 'value')")
+    result = sbx.notebook.exec_cell("import os; os.getenv('FOO')", envs={"FOO": "baz"})
+    assert result.text == "baz"
+
+    result = sbx.notebook.exec_cell("import os; os.getenv('RUNTIME_ENV')")
+    assert result.text == "value"
+
+    result = sbx.notebook.exec_cell("import os; os.getenv('SBX')")
+    assert result.text == "value"
+
+    result = sbx.notebook.exec_cell("import os; os.getenv('FOO')")
+    assert result.text == "bar"
+
     sbx.kill()
