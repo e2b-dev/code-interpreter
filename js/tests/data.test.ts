08c2420bd6fa37141253fe9c52c211ac20debf34
Jakub Novak <jakub@e2b.dev>
2024-08-28 16:05:01 +0200

    Fix missing import in JS test

--- js/tests/data.test.ts
+++ js/tests/data.test.ts
@@ -3,7 +3,10 @@ import { expect } from 'vitest'
 import { sandboxTest } from './setup'
 
 sandboxTest('get data', async ({ sandbox }) => {
-  const execution = await sandbox.notebook.execCell('pd.DataFrame({"a": [1, 2, 3]})')
+  const execution = await sandbox.notebook.execCell(`
+import pandas as pd
+pd.DataFrame({"a": [1, 2, 3]})
+`)
 
   const result = execution.results[0]
   expect(result.data).toBeDefined()
