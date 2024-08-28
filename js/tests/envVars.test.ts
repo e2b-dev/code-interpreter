5bda38a7e661d3084b4fed39a6137d3b67d06315
Jakub Novak <jakub@e2b.dev>
2024-08-28 13:37:22 +0200

    Fix JS tests

--- js/tests/envVars.test.ts
+++ js/tests/envVars.test.ts
@@ -5,7 +5,7 @@ import { CodeInterpreter } from "../src";
 
 // Skip this test if we are running in debug mode — the pwd and user in the testing docker container are not the same as in the actual sandbox.
 sandboxTest('env vars', async () => {
-  const sandbox = await CodeInterpreter.create('g0zptwsuemevq896f2u3', { envs: { TEST_ENV_VAR: "supertest" } })
+  const sandbox = await CodeInterpreter.create({ envs: { TEST_ENV_VAR: "supertest" } })
   const result = await sandbox.notebook.execCell(`import os; x = os.getenv('TEST_ENV_VAR'); x`)
 
   expect(result.results[0].text.trim()).toEqual('supertest')
@@ -18,7 +18,7 @@ sandboxTest('env vars on sandbox', async ({sandbox}) => {
 })
 
 sandboxTest('env vars on sandbox override', async () => {
-  const sandbox = await CodeInterpreter.create('g0zptwsuemevq896f2u3', { envs: { FOO: "bar", SBX: "value" } })
+  const sandbox = await CodeInterpreter.create({ envs: { FOO: "bar", SBX: "value" } })
   await sandbox.notebook.execCell("import os; os.environ['FOO'] = 'bar'; os.environ['RUNTIME_ENV'] = 'value'")
   const result = await sandbox.notebook.execCell("import os; os.getenv('FOO')", {envs: {FOO: "baz"}})
 
