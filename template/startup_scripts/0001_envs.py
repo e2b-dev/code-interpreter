3cc466e0b5a60b8a596a2180feb0cecb3155889a
Jakub Novak <jakub@e2b.dev>
2024-09-05 14:09:42 +0200

    Fix resetting envs

--- template/startup_scripts/0001_envs.py
+++ template/startup_scripts/0001_envs.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import IPython
 
@@ -30,9 +31,13 @@ class E2BEnviron(os._Environ):
         self.keys_to_remove = keys_to_remove
 
     def reset_envs_for_execution(self):
-        for key in self.keys_to_remove:
+        keys_to_remove = copy.copy(self.keys_to_remove)
+        for key in keys_to_remove:
             self.pop(key)
-        self.update(self.return_values)
+
+        return_values = copy.copy(self.return_values)
+        self.update(return_values)
+
         self.keys_to_remove.clear()
         self.return_values.clear()
 
@@ -48,8 +53,12 @@ e2b_environ.update(os.environ)
 os.environ = e2b_environ
 
 
-def reset_envs(_):
-    os.environ.reset_envs_for_execution()
+def reset_envs(*args, **kwargs):
+    try:
+        if isinstance(os.environ, E2BEnviron):
+            os.environ.reset_envs_for_execution()
+    except Exception as e:
+        print(f"Failed to reset envs: {e}")
 
 
 ip = IPython.get_ipython()
