26517a84563b650db33fd8b3c72c4ebd157045ef
Jakub Novak <jakub@e2b.dev>
2024-10-16 03:48:52 -0700

    [skip CI] Remove console log

--- js/tests/defaultKernels.test.ts
+++ js/tests/defaultKernels.test.ts
@@ -4,6 +4,5 @@ import { sandboxTest } from './setup'
 
 sandboxTest('test js kernel', async ({ sandbox }) => {
   const output = await sandbox.runCode('console.log("Hello World!")', { language: 'js' })
-  console.log(output)
   expect(output.logs.stdout).toEqual(['Hello World!\n'])
 })
