77d7a8f06c83d8395685b7011b09b34f86617a42
Mish <10400064+mishushakov@users.noreply.github.com>
2025-03-25 17:51:13 +0100

    added Deno warmup script

--- template/start-up.sh
+++ template/start-up.sh
@@ -13,17 +13,28 @@ function start_jupyter_server() {
 		response=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8888/api/status")
 	done
 
-	response=$(curl -s -X POST "localhost:8888/api/sessions" -H "Content-Type: application/json" -d '{"path": "/home/user", "kernel": {"name": "python3"}, "type": "notebook", "name": "default"}')
-	status=$(echo "${response}" | jq -r '.kernel.execution_state')
-	if [[ ${status} != "starting" ]]; then
-		echo "Error creating kernel: ${response} ${status}"
+	python_response=$(curl -s -X POST "localhost:8888/api/sessions" -H "Content-Type: application/json" -d '{"path": "/home/user", "kernel": {"name": "python3"}, "type": "notebook", "name": "default"}')
+	python_status=$(echo "${python_response}" | jq -r '.kernel.execution_state')
+	if [[ ${python_status} != "starting" ]]; then
+		echo "Error creating kernel: ${python_response} ${python_status}"
 		exit 1
 	fi
 
 	sudo mkdir -p /root/.jupyter
-	kernel_id=$(echo "${response}" | jq -r '.kernel.id')
-	sudo echo "${kernel_id}" | sudo tee /root/.jupyter/kernel_id >/dev/null
-	sudo echo "${response}" | sudo tee /root/.jupyter/.session_info >/dev/null
+	python_kernel_id=$(echo "${python_response}" | jq -r '.kernel.id')
+	sudo echo "${python_kernel_id}" | sudo tee /root/.jupyter/kernel_id >/dev/null
+	sudo echo "${python_response}" | sudo tee /root/.jupyter/.session_info >/dev/null
+
+	# Warm up the Deno kernel
+	deno_response=$(curl -s -X POST "localhost:8888/api/sessions" -H "Content-Type: application/json" -d '{"path": "/home/user/deno", "kernel": {"name": "deno"}, "type": "notebook", "name": "default"}')
+	deno_status=$(echo "${deno_response}" | jq -r '.kernel.execution_state')
+	if [[ ${deno_status} != "starting" ]]; then
+		echo "Error creating kernel: ${deno_response} ${deno_status}"
+		exit 1
+	fi
+
+	deno_session_id=$(echo "${deno_response}" | jq -r '.id')
+	curl -s -X DELETE "localhost:8888/api/sessions/${deno_session_id}"
 
 	cd /root/.server/
 	/root/.server/.venv/bin/uvicorn main:app --host 0.0.0.0 --port 49999 --workers 1 --no-access-log --no-use-colors
