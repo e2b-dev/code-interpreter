9f44d34eb716f9776d3f7b673afae4b18ecc3a4f
Mish <10400064+mishushakov@users.noreply.github.com>
2025-04-30 17:34:56 +0200

    added test for typescript kernel failure

--- js/tests/defaultKernels.test.ts
+++ js/tests/defaultKernels.test.ts
@@ -3,11 +3,23 @@ import { expect } from 'vitest'
 import { sandboxTest } from './setup'
 
 sandboxTest('test js kernel', async ({ sandbox }) => {
-  const output = await sandbox.runCode('console.log("Hello World!")', { language: 'js' })
+  const output = await sandbox.runCode('console.log("Hello World!")', {
+    language: 'js',
+  })
   expect(output.logs.stdout).toEqual(['Hello World!\n'])
 })
 
 sandboxTest('test ts kernel', async ({ sandbox }) => {
-  const output = await sandbox.runCode('const message: string = "Hello World!"; console.log(message)', { language: 'ts' })
+  const output = await sandbox.runCode(
+    'const message: string = "Hello World!"; console.log(message)',
+    { language: 'ts' }
+  )
   expect(output.logs.stdout).toEqual(['Hello World!\n'])
+})
+
+sandboxTest('test ts kernel errors', async ({ sandbox }) => {
+  const output = await sandbox.runCode('import x from "module";', {
+    language: 'typescript',
+  })
+  expect(output.error?.name).toEqual('TypeScriptCompilerError')
 })
