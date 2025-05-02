95163ce29497afb9c1072c899517d0d9002fa2f3
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2025-05-02 14:10:27 +0200

    Added TypeScript support to the code interpreter (#91)
    * added typescript support via SWC
    
    * added ts tests
    
    * changed target node version
    
    * minor nits by @0div
    
    * added test for typescript kernel failure
    
    * changed swc path in prod Dockerfile
    
    * added changeset
    
    * skip test here so that the template can be built

--- js/tests/defaultKernels.test.ts
+++ js/tests/defaultKernels.test.ts
@@ -3,6 +3,8 @@ import { expect } from 'vitest'
 import { sandboxTest } from './setup'
 
 sandboxTest('test js kernel', async ({ sandbox }) => {
-  const output = await sandbox.runCode('console.log("Hello World!")', { language: 'js' })
+  const output = await sandbox.runCode('console.log("Hello World!")', {
+    language: 'js',
+  })
   expect(output.logs.stdout).toEqual(['Hello World!\n'])
 })
