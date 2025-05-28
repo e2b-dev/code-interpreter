63c4bcf2bee792d4e8a7414cb4f43a9f03ddaf77
Mish <10400064+mishushakov@users.noreply.github.com>
2025-05-28 19:28:27 +0200

    added tests for new js kernel

--- js/tests/defaultKernels.test.ts
+++ js/tests/defaultKernels.test.ts
@@ -9,6 +9,34 @@ sandboxTest('test js kernel', async ({ sandbox }) => {
   expect(output.logs.stdout).toEqual(['Hello World!\n'])
 })
 
+sandboxTest('test esm imports', async ({ sandbox }) => {
+  const output = await sandbox.runCode(
+    `
+    import { readFileSync } from 'fs'
+    console.log(typeof readFileSync)
+    `,
+    {
+      language: 'js',
+    }
+  )
+  expect(output.logs.stdout).toEqual(['function\n'])
+})
+
+sandboxTest(
+  'test top-level await and promise resolution',
+  async ({ sandbox }) => {
+    const output = await sandbox.runCode(
+      `
+    await Promise.resolve('Hello World!')
+    `,
+      {
+        language: 'js',
+      }
+    )
+    expect(output.text).toEqual('Hello World!')
+  }
+)
+
 sandboxTest('test ts kernel', async ({ sandbox }) => {
   const output = await sandbox.runCode(
     'const message: string = "Hello World!"; console.log(message)',
--- python/tests/async/test_async_default_kernels.py
+++ python/tests/async/test_async_default_kernels.py
@@ -7,6 +7,20 @@ async def test_js_kernel(async_sandbox: AsyncSandbox):
     )
     assert execution.logs.stdout == ["Hello, World!\n"]
 
+async def test_js_esm_imports(async_sandbox: AsyncSandbox):
+    execution = await async_sandbox.run_code("""
+    import { readFileSync } from 'fs'
+    console.log(typeof readFileSync)
+    """, language="js")
+    assert execution.logs.stdout == ["function\n"]
+
+
+async def test_js_top_level_await(async_sandbox: AsyncSandbox):
+    execution = await async_sandbox.run_code("""
+    await Promise.resolve('Hello World!')
+    """, language="js")
+    assert execution.text == "Hello World!"
+
 
 async def test_ts_kernel(async_sandbox: AsyncSandbox):
     execution = await async_sandbox.run_code(
