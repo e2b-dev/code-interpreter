7673c9f73567ecb2eed9911c51f89c7cf8ed802d
Mish <10400064+mishushakov@users.noreply.github.com>
2025-11-04 15:26:13 +0100

    generate template alias before test run

--- template/build_ci.py
+++ template/build_ci.py
@@ -1,10 +1,10 @@
+import os
 from e2b import Template, default_build_logger
 from template import make_template
-from uuid import uuid4
 
 Template.build(
     make_template(set_user_workdir=True),
-    alias="code-interpreter-ci-" + str(uuid4()),
+    alias=os.environ["E2B_TESTS_TEMPLATE"],
     cpu_count=2,
     memory_mb=2048,
     on_build_logs=default_build_logger(),
