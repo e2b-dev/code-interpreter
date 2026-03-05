5ab28433d09bb57b728aa9d4a8628c5810b9fe91
Matt Brockman <matt.brockman@e2b.dev>
2026-03-04 20:29:50 -0800

    outdated template id deletion

--- template/build_ci.py
+++ template/build_ci.py
@@ -2,10 +2,18 @@ import os
 from e2b import Template, default_build_logger
 from template import make_template
 
-Template.build(
+build_info = Template.build(
     make_template(),
     alias=os.environ["E2B_TESTS_TEMPLATE"],
     cpu_count=2,
     memory_mb=2048,
     on_build_logs=default_build_logger(),
 )
+
+template_id = build_info.template_id
+print(f"Built template ID: {template_id}")
+
+github_output = os.getenv("GITHUB_OUTPUT")
+if github_output:
+    with open(github_output, "a", encoding="utf-8") as fh:
+        fh.write(f"template_id={template_id}\n")
