c45426294f4a9de1149fff2b5f48c9f2cac23ede
Matt Brockman <matt.brockman@e2b.dev>
2026-03-05 10:43:18 -0800

    add workflow dispatch to deploy the template (#211)
    * build prod template has a workflow dispatch

--- template/build_prod.py
+++ template/build_prod.py
@@ -1,13 +1,18 @@
+import os
+
 from dotenv import load_dotenv
 from e2b import Template, default_build_logger
 from template import make_template
 
 load_dotenv()
 
+skip_cache = os.getenv("SKIP_CACHE", "false").lower() == "true"
+
 Template.build(
     make_template(),
     alias="code-interpreter-v1",
     cpu_count=2,
     memory_mb=2048,
+    skip_cache=skip_cache,
     on_build_logs=default_build_logger(),
 )
