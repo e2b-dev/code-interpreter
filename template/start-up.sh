2f588c615fee82cbd2aeb377f76f1dc22b706139
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2026-03-18 16:43:19 +0100

    added systemd oom handling and tests

--- template/start-up.sh
+++ template/start-up.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-function start_jupyter_server() {
-	counter=0
-	response=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8888/api/status")
-	while [[ ${response} -ne 200 ]]; do
-		let counter++
-		if ((counter % 20 == 0)); then
-			echo "Waiting for Jupyter Server to start..."
-			sleep 0.1
-		fi
-
-		response=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8888/api/status")
-	done
+function start_code_interpreter() {
+	/root/.jupyter/jupyter-healthcheck.sh
+	if [ $? -ne 0 ]; then
+		echo "Jupyter Server failed to start, aborting."
+		exit 1
+	fi
 
 	cd /root/.server/
 	.venv/bin/uvicorn main:app --host 0.0.0.0 --port 49999 --workers 1 --no-access-log --no-use-colors --timeout-keep-alive 640
 }
 
 echo "Starting Code Interpreter server..."
-start_jupyter_server &
+start_code_interpreter &
 MATPLOTLIBRC=/root/.config/matplotlib/.matplotlibrc jupyter server --IdentityProvider.token="" >/dev/null 2>&1
