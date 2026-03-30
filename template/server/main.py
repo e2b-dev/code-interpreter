75174845aca2e7db7e8c77ca248c1220355ad982
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2026-03-30 11:27:47 +0200

    removed credentials true

--- template/server/main.py
+++ template/server/main.py
@@ -64,7 +64,6 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
